d7d4c4fb1899c390c3b37c1b6c6cad1b5461f01f
Nathan Lee <nathan.l.lee123@gmail.com>
2019-04-06 00:46:27 -0700

    Added a git  submodule

---
+++ frontend/build/precache-manifest.b71ab7f1c6bcb674ad0932863fd4ec56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10e28a27348b6afdec3c",
+    "url": "/Litsearch/static/js/runtime~main.10e28a27.js"
+  },
+  {
+    "revision": "e7501f6f32e1b8d5e11a",
+    "url": "/Litsearch/static/js/main.e7501f6f.chunk.js"
+  },
+  {
+    "revision": "916ee2d953dc41de155c",
+    "url": "/Litsearch/static/js/1.916ee2d9.chunk.js"
+  },
+  {
+    "revision": "e7501f6f32e1b8d5e11a",
+    "url": "/Litsearch/static/css/main.ecd71745.chunk.css"
+  },
+  {
+    "revision": "916ee2d953dc41de155c",
+    "url": "/Litsearch/static/css/1.381b3e06.chunk.css"
+  },
+  {
+    "revision": "882b0c8c7b2c67171c3bb1c157b7a095",
+    "url": "/Litsearch/index.html"
+  }
+];
