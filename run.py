6899878a6d094406d8d5438877a8d93afeddfb79
Nathan Lee <nathan.l.lee123@gmail.com>
2019-04-23 23:40:42 -0700

    A complete refactor

---
+++ run.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import render_template, request, jsonify
+
+#from flask_restplusu import Api
+
+import csv
+import json
+from sklearn.datasets import make_classification
+import pickle
+from nltk import sent_tokenize
+
+from templates import app
+#Load this config object for development mode
+app.config.from_object('configurations.DevelopmentConfig')
+app.run()
+
+#model stuff
+#clf = pickle.load(open('model', 'rb'))
+
+testdata = {
+    'theme': 'family',
+    'quotes': ['Blah Blah Blah', 'Lorem ipsum']
+}
+
+app = Flask(__name__)
+#api = Api(app)
+
+@app.route('/')
+def predict():
+    data = request.get_json(force=True)
+    print(data)
+    return testdata
+
+if __name__ == "__main__":
+    app.run(port = 5001)
